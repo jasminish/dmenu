b51152455be5b67a7705bfb381c1c13e06eaa864
Jasmine Ang <jasminishh@gmail.com>
2020-11-14 00:42:53 +0800

    initial customization

---
+++ README.md
@@ -0,0 +1,5 @@
+# dmenu
+
+Patches 
+* [center](https://tools.suckless.org/dmenu/patches/center/dmenu-center-20200111-8cd37e1.diff) 
+* [lineheight](https://tools.suckless.org/dmenu/patches/line-height/dmenu-lineheight-5.0.diff)
--- config.def.h
+++ config.def.h
@@ -2,19 +2,21 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
+static const char *fonts[] = { "Hack:size=10" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#f0f0f0", "#202020" },
+	[SchemeSel] = { "#f0f0f0", "#454545" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 8;
+static unsigned int lineheight = 30;
+static unsigned int min_lineheight = 30;
 
 /*
  * Characters not considered part of a word while deleting words
